0bff9164a7ecbdd9f7fc33efa5651dc15fe9e8cc
matias-romero-traychi <matias.romero@traychi.ar>
2025-08-31 12:03:03 -0300

    AJUSTE EN MANEJO DE COMPONENTE BANNER

--- src/app/page.js
+++ src/app/page.js
@@ -4,16 +4,15 @@ import Button from "@/components/Button/Button";
 export default function Home() {
   return (
     <div className="w-100">
-      <Banner 
+      <Banner
         title={"Gestión y Telemedicina para la Clínica Moderna"} 
         text={"Centraliza agendas y pacientes, y conecta con ellos a través de videollamadas seguras. Todo en un solo lugar."} 
-        children={
+      >
           <div className="w-100 text-center mt-5">
             <Button type={"action"}>Empieza Gratis</Button>
             <Button type={"classic-cream"}>Ver Soluciones</Button>
           </div>
-        }
-      />
+      </Banner>
     </div>
   );
 }
